364d9cd7a5dd593e1fdfcb9b6e0d07dba3466257
SHREEJIT SANJAY JADHAV <48149968+DINVSTR@users.noreply.github.com>
2022-07-02 15:10:59 +0530

    Add files via upload

---
+++ oops_assessment_1/Admin_Department.java
@@ -0,0 +1,14 @@
+package oops_assessment_1;
+
+public class Admin_Department {
+	public String departmentName() {
+		return "Admin departemnt";
+	}
+	public String getTodaysWork() {
+		return "Complete your documents Submission" ;
+	}
+	public String getWorkDeadline() {
+		return "Complete by EOD ";
+	}
+		
+}
---
+++ oops_assessment_1/Tech_Department.java
@@ -0,0 +1,16 @@
+package oops_assessment_1;
+
+public class Tech_Department {
+	public String departmentName() {
+		return "Tech Departemnt";
+	}
+	public String getTodaysWork() {
+		return "Complete coding of module 1";
+	}
+	public String getWorkDeadline() {
+		return "Complete by EOD";
+	}
+	public String getTechStackInformation() {
+		return "core Java";
+	}
+}
